6e4850f4a6e716262f84925d5c8fb96e62439cba
Douglas Worley <fauxserve@icloud.com>
2016-04-07 16:29:16 -0700

    extension attribute

--- ea_sshUsers.sh
+++ ea_sshUsers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 clear
 
 theGroups=$(/usr/bin/dscl localhost -read /Local/Default/Groups/com.apple.access_ssh NestedGroups | cut -c 17-)
@@ -16,3 +16,53 @@ done
 echo "<result>`readTheGroups $theGroups`</result>"
 
 
+#!/bin/bash 
+#
+####################################################################################################
+#
+# Copyright (c) 2013, JAMF Software, LLC.  All rights reserved.
+#
+#       This script was written by the JAMF Software Profesional Services Team for the 
+#		 JumpStart
+#
+#       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
+#       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#       DISCLAIMED. IN NO EVENT SHALL JAMF SOFTWARE, LLC BE LIABLE FOR ANY
+#       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#       ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################################################
+#
+# SUPPORT FOR THIS PROGRAM
+#
+#       This program is distributed "as is" by JAMF Software, Professional Services Team. For more
+#       information or support for this script, please contact your JAMF Software Account Manager.
+#
+#####################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#	ea_sshUsers.sh
+#
+# SYNOPSIS - How to use
+#	
+# 
+# Extension Attribute to find the list of current groups that can access the local Mac via SSH.
+# 
+# 
+# 
+####################################################################################################
+#
+# HISTORY
+#
+#	Version: 1.0
+#
+#	- Created by Douglas Worley, Senior Professional Services Engineer, JAMF Software on April 7 2016
+#
+####################################################################################################
